2635d0b31d1647d5a2951aa225b774a5d26d2a78
senakrts <50340200+senakrts@users.noreply.github.com>
2020-04-07 13:10:23 +0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        list1.add(100);
+        list1.add(200);
+        list1.add(300);
+        ArrayOrganizer a;
+        ArrayList<Integer> c = a.concatAndSort(list1, list2, list3);
+    }
+}
